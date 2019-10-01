a863cb294c87f4dedcec8260678149a380054cb1
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-01 23:47:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,8 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    puts "Hi, my name is Sophie."
-    "Hi, my name is Sophie.".end_with?("Hi, my name is Sophie.", "Hi, my name is Sophie")
+    self.end_with(".")
   end
 
   def question?
